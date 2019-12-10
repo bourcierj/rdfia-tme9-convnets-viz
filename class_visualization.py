b70d3302ffc25bb83b2cbfb122fb26ee21617925
bourcierj <julesbourcier1@gmail.com>
2019-12-10 18:27:02 +0100

    Add more options for regularization

--- class_visualization.py
+++ class_visualization.py
@@ -19,22 +19,29 @@ from utils import *
 
 def create_class_visualization(target_y, model, dtype, init_img=None, l2_reg=1e-3,
                                learning_rate=5, num_iterations=200, blur_every=10,
-                               max_jitter=16, show_every=25, class_names=None,
-                               savepath=None):
+                               blur_width=0.5, max_jitter=16, clamp=True,
+                               show_every=25, class_names=None, savepath=None):
     """
     Generate an image to maximize the score of target_y under a pretrained model.
     Shows the progress every `show_every` iterations.
     Args:
         target_y (int): integer in the range [0, 1000) giving the index of the class
-        model (torch.nn.Module): a pretrained CNN that will be used to generate the image
+        model (torch.nn.Module): a pretrained CNN that will be used to generate
+            the image
         dtype : torch datatype to use for computations
 
         init_img (torch.Tensor): initial image to use (if None, will be random)
         l2_reg (float): strength of L2 regularization on the image
         learning_rate (float): how big of a step to take
         num_iterations (int): how many iterations to use
-        blur_every (int): how often to blur the image as an implicit regularizer (if None, does not blur the image)
-        max_jitter (int): how much to jitter the image as an implicit regularizer (if None, does not jitter the image)
+        blur_every (int): how often to blur the image as an implicit regularizer
+            (if None, does not blur the image)
+        blur_width (float): the width of the blurring filter applied
+            (if `blur_every` is None, it is ignored)
+        max_jitter (int): maximum value to randomly jitter the image as an implicit
+            regularizer (if None, does not jitter the image)
+        clamp (int): if True, clamps the image to standardize it a bit as an implicit
+            regularizer
         show_every (int): How often to show the intermediate result
 
     Returns:
@@ -70,12 +77,13 @@ def create_class_visualization(target_y, model, dtype, init_img=None, l2_reg=1e-
             img.data.copy_(jitter(img.data, -ox, -oy))
 
         # as regularizer, clamp and periodically blur the image
-        for c in range(3):
-            lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
-            hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
-            img.data[:, c].clamp_(min=lo, max=hi)
+        if clamp:
+            for c in range(3):
+                lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
+                hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
+                img.data[:, c].clamp_(min=lo, max=hi)
         if blur_every and t % blur_every == 0:
-            img.data = blur_image(img.data.cpu().numpy(), sigma=0.5)
+            img.data = blur_image(img.data.cpu().numpy(), sigma=blur_width)
 
         # Periodically show the image
         if t == 0 or (t + 1) % show_every == 0 or t == num_iterations - 1:
@@ -84,14 +92,15 @@ def create_class_visualization(target_y, model, dtype, init_img=None, l2_reg=1e-
 
             plt.imshow(deprocess(img.clone().cpu()))
             class_name = class_names[target_y] if class_names else ''
-            plt.title("{}\nIteration {} \nConfidence: {:.2f}%"
-                      .format(class_name, t + 1, target_y_score*100))
+            plt.title("{}\nIteration {} \nConfidence: {:.2%}"
+                      .format(class_name, t + 1, target_y_score))
             plt.gcf().set_size_inches(6, 6)
             plt.gcf().tight_layout()
             plt.axis('off')
             if t != num_iterations - 1:
                 plt.show()
-    if savepath: plt.savefig(savepath)
+
+    if savepath: plt.savefig(savepath, bbox_inches='tight')
     plt.show()
 
     return deprocess(img.cpu())
